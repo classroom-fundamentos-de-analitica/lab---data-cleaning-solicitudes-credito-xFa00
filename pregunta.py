0b2a882858b1b07f6181d86686cf14fa5c49b320
Fabian Andres Chiran <111092524+xFa00@users.noreply.github.com>
2022-11-28 01:40:16 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,4 +17,16 @@ def clean_data():
     # Inserte su código aquí
     #
 
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
+    df=df.drop(['Unnamed: 0'], axis=1)
+    df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]]=df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
+    df=df.replace(to_replace="(_)|(-)",value=" ",regex=True)    
+    df=df.replace(to_replace="[,$]|(\.00$)",value="",regex=True)
+    df.monto_del_credito = df.monto_del_credito.astype("int")
+    df.comuna_ciudadano = df.comuna_ciudadano.astype("float")
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,infer_datetime_format=True,errors='ignore',dayfirst=True)
+    df.fecha_de_beneficio = df.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
+    df.drop_duplicates(inplace = True)
+
     return df
